e16eda6249575c6c5a9ca0d9ed9f1239638a86a7
lai2-24 <lai2-24@itismeucci.com>
2023-12-19 13:40:10 +0100

    fix server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -18,29 +20,38 @@ public class App
     {
         try {
             ServerSocket serverSocket = new ServerSocket(8080);
+            System.out.println("server avviato");
+
             while (true) {
-                Socket client = serverSocket.accept();
-                BufferedReader inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                String in = "";
-                String percorso = "";
-                String ext = "";
+                Socket s = serverSocket.accept();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+                String line;
+                line = in.readLine();
+                String filePath = line.split(" ")[1];
+
                 do {
-                    in = inDalClient.readLine();
-                    System.out.println(in);
-                    if (percorso.isEmpty() && (!in.isEmpty() || !in.equals(null))) {
-                        percorso = in.split(" ")[1];
-                        if (!percorso.startsWith("/")) {
-                            percorso = "/" + percorso;
-                        }
-                        percorso = "htdocs" + percorso;
-                        ext = percorso.split("\\.")[1];
-                        percorso = percorso.split("\\.")[0];
-                    }
-                    if (in.isEmpty() || in.equals(null)) {
-                        break;
-                    }
-                } while (true);
-                sendFile(client, percorso + "." + ext, ext);
+                    line = in.readLine();
+                    System.out.println(line);
+                } while (!line.isEmpty());
+
+                File file  = new File("htdocs" + filePath);
+                if (file.exists()) {
+                    sendBinaryFile(out, file);
+                } else {
+                    String msg = "File non trovato";
+                    out.writeBytes("HTTP/1.1 404 Not Found\n");
+                    out.writeBytes("Content-Length: " + msg.length() + "\n");
+                    out.writeBytes("Server: Java HTTP Server from Grandi: 1.0" + "\n");
+                    out.writeBytes("Date: " + new Date() + "\n");
+                    out.writeBytes("Content-Type: text/plain; charset=utf-8\n");
+                    out.writeBytes("\n");
+                    out.writeBytes(msg);
+                }
+
+                s.close();
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -49,74 +60,44 @@ public class App
         }  
     }
     
-    private static void sendFile(Socket s, String file, String extFile) {
-        try {
-            try {
-                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+    private static void sendBinaryFile(DataOutputStream out, File file) throws IOException{
+        out.writeBytes("HTTP/1.1 200 OK" + "\n");
+        out.writeBytes("Content-Length: " + file.length() + "\n");
+        out.writeBytes("Server: Java HTTP Server from Grandi: 1.0" + "\n");
+        out.writeBytes("Date: " + new Date() + "\n");
+        out.writeBytes("Content-Type: " + getContentType(file) + "charset=utf-8\n");
+        out.writeBytes("\n");
 
-                File myObj = new File(file);
-                FileInputStream in = new FileInputStream(file);
-
-                out.writeBytes("HTTP/1.1 200 OK" + "\n");
-                System.out.println(myObj.length());
-                out.writeBytes("Content-Length: " + myObj.length() + "\n");
-                out.writeBytes("Server: Java HTTP Server from Grandi: 1.0" + "\n");
-                out.writeBytes("Date: " + new Date(0) + "\n");
-                out.writeBytes("Content-Type: " + getContentType(extFile) + "\n");
-
-                out.writeBytes("\n");
-
-                byte[] buf = new byte[8192];
-                int n;
-
-                while ((n = in.read(buf)) != -1) {
-                    out.write(buf, 0, n);
-                }
-
-                out.close();
-                in.close();
-
-            } catch (Exception e) {
-                DataOutputStream out = new DataOutputStream(s.getOutputStream());
-                out.writeBytes("HTTP/1.1 404 Not found" + "\n");
-            }
-        } catch (Exception e) {
-            System.out.println("errore");
+        InputStream input = new FileInputStream(file);
+        byte[] buf = new byte[8192];
+        int n;
+        while ((n = input.read(buf)) != -1) {
+            out.write(buf, 0, n);
         }
+        input.close();
     }
 
-    private static String getContentType(String ext) {
-        String type = "";
-
+    private static String getContentType(File f) {
+       String [] s = f.getName().split("\\.");
+       String ext = s[s.length-1];
         switch (ext) {
             case "html":
-                type = "text/html;" + "charset=utf-8";
-            break;
+                return "text/html;";
            
-            case "jpg":
             case "png":
-            case "jpeg":
-            case "webp":
-                type = "image/" + ext;
-            break;
-            
+                return "image/png;";
+
             case "css":
-                type = "text/html";
-            break;
+                return "text/html;";
 
             case "js":
-                type = "application/js";
-            break;
+                return "application/js;";
 
             case "ico":
-                type = "favicon/ico";
-            break;
+                return "favicon/ico;";
 
             default:
-                type = "text/plain";
-            break;
+                return "";
         }
-
-        return type;
     }
 }
